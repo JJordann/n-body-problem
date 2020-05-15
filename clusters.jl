a57cb3ad47bc951207cc655397a1924212b174c0
CitizenIsErased <timi.sp@gmail.com>
2020-05-15 14:03:18 +0200

    object velocity scalling

--- clusters.jl
+++ clusters.jl
@@ -3,6 +3,9 @@ using LinearAlgebra
 using Images
 using Plots
 
+function vector_length(x, y, z)
+	return sqrt(x*x  + y*y + z*z) 
+end
 
 function generate_starting_conditions(cluster_number, object_number)
 	x_together = []
@@ -12,6 +15,8 @@ function generate_starting_conditions(cluster_number, object_number)
 	x_vel_together = []
 	y_vel_together = []
 	z_vel_together = []
+	
+	m_together = []
 	
 	for cluster in 1:cluster_number
 		#generate center of the cluster
@@ -30,14 +35,30 @@ function generate_starting_conditions(cluster_number, object_number)
 			zVel = 0
 			yVel = - xVel * xVel / y_coordinates[k]
 			
-			#make it smaller
-			xVel = xVel / 20
-			yVel = yVel / 20
+			#velocity magnitude adjustment
+			vectorL = vector_length(xVel, yVel, zVel)
+			r = vector_length(x_coordinates[k], y_coordinates[k], z_coordinates[k])
+			center_mass = 10.0
+			desired_length = sqrt(center_mass / r)
+			
+			multiplyer = vectorL / desired_length
+			xVel = xVel / multiplyer
+			yVel = yVel / multiplyer
+			
 			
 			append!(x_vel_together, xVel)
 			append!(y_vel_together, yVel)
 			append!(z_vel_together, zVel)
+			
+			#mass = vector_length(xVel, yVel, zVel) * vector_length(xVel, yVel, zVel) * vector_length(x_coordinates[k], y_coordinates[k], z_coordinates[k])
+			
+			append!(m_together, 0.00000000000001)
+			
 		end
+		
+		#center of the cluster
+		append!(m_together, 10.0)
+		
 		
 		append!(x_vel_together, 0)
 		append!(y_vel_together, 0)
@@ -60,7 +81,7 @@ function generate_starting_conditions(cluster_number, object_number)
 		
 	end
 	
-	return x_together, y_together, z_together, x_vel_together, y_vel_together, z_vel_together 
+	return x_together, y_together, z_together, x_vel_together, y_vel_together, z_vel_together, m_together
 end
 
 
@@ -70,16 +91,16 @@ n_of_clusters = parse(UInt8, readline())
 println("enter number of object per cluster")
 n_of_objects_per_cluster = parse(UInt8, readline())
 
-pos_x, pos_y, pos_z, vel_x, vel_y, vel_z = generate_starting_conditions(n_of_clusters, n_of_objects_per_cluster)
+pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, M = generate_starting_conditions(n_of_clusters, n_of_objects_per_cluster)
 pos = [pos_x' ; pos_y' ; pos_z']'
 vel = [vel_x' ; vel_y' ; vel_z']'
 
-G = 2.95912208286e-4
+G = 1
 N = n_of_clusters * n_of_objects_per_cluster
 
 # image size
 n = 512
-dt = 1;
+dt = 3;
 iters = 100;
 
 scale = p -> round.(p .* 3 .+ n/2)
@@ -110,7 +131,7 @@ for iter in 1:iters
 	toImage(pos, iter);
     for i in 1:N, j in 1:N
         if i != j
-            acc[i, :] += (G ./ norm(pos[j, :] - pos[i, :]) ^ 3) .* (pos[j, :] - pos[i, :])
+            acc[i, :] += (G * M[j] ./ norm(pos[j, :] - pos[i, :]) ^ 3) .* (pos[j, :] - pos[i, :])
         end
     end
     global vel = vel .+ (acc .* dt)
